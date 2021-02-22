dcb9546d284b0163cc6bf0b2adeb3b75c1a59dcf
OleksandrChervinskyi <OleksandrChervinskyi2@gmail.com>
2021-02-22 15:15:48 +0200

    all exept *****

--- lessonn js 3/hw-rules.js
+++ lessonn js 3/hw-rules.js
@@ -6,19 +6,102 @@
 // створити скрипт, котрий зчитує всі заголовки, та робить в блоці з id=content з них список(ul>li), який буде змістом того, що знаходиться на сторінці.
 // Скріпт повинен працювати навіть якщо кількість блоків з заголовком та параграфом зміниться.
 
+let contents = document.getElementById('content');
+let ul = document.createElement('ul');
+contents.appendChild(ul)
 
+let allTitles = document.getElementsByTagName('h2');
+for (const i of allTitles) {
+    let item = document.createElement('li');
+    item.innerText = i.innerText;
 
-let table = document.createElement('ul');
-table.id = 'content';
-let allH2Tags = document.getElementsByTagName('h2');
-for (const i of allH2Tags) {
-    console.log(i);
-    console.log(typeof(i));
-    let li = document.createElement('li');
-    li.textContent = i;
-
-    table.appendChild(li);
+    ul.appendChild(item);
 }
 
+// -Є масив котрий характеризує правила. Створити скрипт який ітерує цей масив, та робить з кожне правило в окремому блоці.
+// При цому в блоці, номер правила записати в свій блок, текст правила записати в свій окремий блок.
+// Результатом відпрацювання скріпта повинна бути структура яка міститься в блоці wrap файла rule.html
 
-document.body.appendChild(table)
+// let rules = [{
+//         title: 'Первое правило Бойцовского клуба.',
+//         body: 'Никому не рассказывать о Бойцовском клубе.'
+//     },
+//     {
+//         title: 'Второе правило Бойцовского клуба.',
+//         body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+//     },
+//     {
+//         title: 'Третье правило Бойцовского клуба.',
+//         body: 'В схватке участвуют только двое.'
+//     },
+//     {
+//         title: 'Четвертое правило Бойцовского клуба.',
+//         body: 'Не более одного поединка за один раз.'
+//     },
+//     {
+//         title: 'Пятое правило Бойцовского клуба.',
+//         body: 'Бойцы сражаются без обуви и голые по пояс.'
+//     },
+//     {
+//         title: 'Шестое правило Бойцовского клуба.',
+//         body: 'Поединок продолжается столько, сколько потребуется.'
+//     },
+//     {
+//         title: 'Седьмое правило Бойцовского клуба.',
+//         body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+//     },
+//     {
+//         title: 'Восьмое и последнее правило Бойцовского клуба.',
+//         body: 'Новичок обязан принять бой.'
+//     },
+
+// ];
+
+// let wrapHW = document.createElement('div');
+// wrapHW.className = 'wrap-hw';
+
+// for (const i of rules) {
+//     // create wrap
+//     let rule = document.createElement('div');
+//     rule.className = 'rule';
+//     // create title
+//     let title = document.createElement('h3');
+//     title.className = 'rule_title';
+//     title.innerText = i.title;
+//     // create text
+//     let text = document.createElement('p');
+//     text.className = 'rule_text';
+//     text.innerText = i.body;
+
+//     // add all element to each other
+//     rule.appendChild(title);
+//     rule.appendChild(text);
+//     wrapHW.appendChild(rule);
+// }
+
+// document.body.appendChild(wrapHW);
+
+// *** за допомогою fetch (як в прикладі) отримати від jsonplaceholder всі users. За допомогою document.createElement вивести їх в браузер. Помістити кожен окремий об'єкт в блок, при цьому кожен внутрішній об'єкт в свій блок (блок в блоці).
+// *** за допомогою fetch (як в прикладі) отримати від jsonplaceholder всі posts. За допомогою document.createElement вивести їх в браузер. Помістити кожен окремий об'єкт в блок, при цьому кожен внутрішній об'єкт(якщо він існує) в свій блок (блок в блоці).
+// *** за допомогою fetch (як в прикладі) отримати від jsonplaceholder всі comments. За допомогою document.createElement вивести їх в браузер. Помістити кожен окремий об'єкт в блок, при цьому кожен внутрішній об'єкт(якщо він існує) в свій блок (блок в блоці).
+// ****** при помощи fetch (как в примере) получить от jsonplaceholder все posts. Внутри последнего then() сделать еще один fetch который сделает запрос и получит все comments. Объеденить соответсвующий post с соответсвующими comment и вывести в браузер. Подсказка : в каждом comment есть поле postId которое определяет какой комментарий принадлежит какому посту
+
+fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json())
+    .then(value => {
+        // value = []
+        for (const user of value) {
+            // create wrap users
+            let wrapDiv = document.createElement('div');
+            wrapDiv.className = 'users_fetch';
+
+
+
+            document.body.appendChild(wrapDiv);
+
+        }
+
+
+
+
+
+    })
